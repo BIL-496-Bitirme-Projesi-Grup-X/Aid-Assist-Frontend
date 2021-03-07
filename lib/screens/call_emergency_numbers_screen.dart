b80a3e47f2b227e7540bb4477c030fc71925d024
Aybuke KBezirci <aybuke.kbezirci@gmail.com>
2021-03-07 13:02:24 +0300

    Call Emergency Numbers screen and link is added.

---
+++ lib/screens/call_emergency_numbers_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CallEmergencyNumbersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("ALO YARDIM")),
+        body: Center(
+          child: Text("ALO YARDIM"),
+        ));
+  }
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'call_emergency_numbers_screen.dart';
 import 'latest_earthquakes_screen.dart';
 
 class MainScreen extends StatelessWidget {
@@ -23,6 +24,36 @@ class MainScreen extends StatelessWidget {
             padding: const EdgeInsets.all(15),
             child: Text(
               "Anlık Deprem Listesi (Güncel)",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.green.withOpacity(0.7),
+                  Colors.green,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CallEmergencyNumbersScreen(),
+              ),
+            )
+          },
+          splashColor: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Text(
+              "ALO YARDIM",
               style: Theme.of(context).textTheme.headline6,
             ),
             decoration: BoxDecoration(
