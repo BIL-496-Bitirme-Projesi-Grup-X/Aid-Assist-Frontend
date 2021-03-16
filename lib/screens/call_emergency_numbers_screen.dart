c247b5358307cfed661f7ae0a215316d52dc32d5
nurselisar <nurselisarr@gmail.com>
2021-03-16 23:32:17 +0300

    Emergency numbers added to CallEmergencyNumbersScreen

--- lib/screens/call_emergency_numbers_screen.dart
+++ lib/screens/call_emergency_numbers_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aid_assist/ui/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CallEmergencyNumbersScreen extends StatelessWidget {
@@ -5,8 +6,16 @@ class CallEmergencyNumbersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("ALO YARDIM")),
-        body: Center(
-          child: Text("ALO YARDIM"),
-        ));
+        body: SingleChildScrollView(
+            child: Container(
+          child: Column(children: <Widget>[
+            newEmergencyCallCard("ACİL YARDIM","112"),
+            newEmergencyCallCard("İTFAİYE","110"),
+            newEmergencyCallCard("POLİS","155"),
+            newEmergencyCallCard("JANDARMA","156"),
+            newEmergencyCallCard("AFAD","122"),
+            newEmergencyCallCard("ORMAN YANGINLARI","117"),
+          ]),
+        )));
   }
 }
