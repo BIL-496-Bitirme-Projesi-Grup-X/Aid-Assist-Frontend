489ae4fa4fcc86853050cbd71d0d13b8062cd187
Aybuke KBezirci <aybuke.kbezirci@gmail.com>
2021-03-27 23:42:26 +0300

    Authentication and authorization screens are added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,55 @@
+import 'package:aid_assist/screens/auth_screen.dart';
+import 'package:aid_assist/screens/tabs_screen.dart';
 import 'package:flutter/material.dart';
-
-import './screens/tabs_screen.dart';
+import 'package:provider/provider.dart';
 import 'screens/main_screen.dart';
+import './providers/auth.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Aid Assist',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.lime,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText2: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            bodyText1: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            headline6: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-            )),
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => TabsScreen(),
-      },
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(
-          builder: (ctx) => MainScreen(),
-        );
-      },
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Auth(),
+          ),
+        ],
+        child: Consumer<Auth>(
+            builder: (ctx, auth, _) => MaterialApp(
+                  title: 'Aid Assist',
+                  theme: ThemeData(
+                    primarySwatch: Colors.green,
+                    accentColor: Colors.lime,
+                    canvasColor: Color.fromRGBO(255, 254, 229, 1),
+                    fontFamily: 'Raleway',
+                    textTheme: ThemeData.light().textTheme.copyWith(
+                        bodyText2: TextStyle(
+                          color: Color.fromRGBO(20, 51, 51, 1),
+                        ),
+                        bodyText1: TextStyle(
+                          color: Color.fromRGBO(20, 51, 51, 1),
+                        ),
+                        headline6: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'RobotoCondensed',
+                        )),
+                  ),
+                  initialRoute: '/',
+                  routes: {
+                    '/': (ctx) => auth.isAuth
+                        ? TabsScreen()
+                        : FutureBuilder(
+                            future: auth.tryAutoLogin(),
+                            builder: (ctx, authResultSnapshot) => AuthScreen(),
+                          ),
+                  },
+                  onUnknownRoute: (settings) {
+                    return MaterialPageRoute(
+                      builder: (ctx) => MainScreen(),
+                    );
+                  },
+                )));
   }
 }
---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
+import '../models/http_exception.dart';
+
+enum AuthMode { Signup, Login }
+
+class AuthScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
+                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0, 1],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              height: deviceSize.height,
+              width: deviceSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 20.0),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 75.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.green.shade500,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                          )
+                        ],
+                      ),
+                      child: Text(
+                        'Aid Assist',
+                        style: TextStyle(
+                          color:
+                          Theme.of(context).accentTextTheme.headline6.color,
+                          fontSize: 35,
+                          fontFamily: 'Anton',
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: deviceSize.width > 600 ? 2 : 1,
+                    child: AuthCard(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AuthCard extends StatefulWidget {
+  const AuthCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode _authMode = AuthMode.Login;
+  Map<String, String> _authData = {
+    'nationalIdentityNo': '',
+    'password': '',
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Hata Oluştu!'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Tamam'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false).login(
+          _authData['nationalIdentityNo'],
+          _authData['password'],
+        );
+      } else {
+        // Sign user up
+        await Provider.of<Auth>(context, listen: false).signup(
+          _authData['nationalIdentityNo'],
+          _authData['password'],
+        );
+      }
+    } on HttpException catch (error) {
+      var errorMessage = 'Yetkilendirme hatası';
+      if (error.toString().contains('ID_EXISTS')) {
+        errorMessage =
+            'Bu TC Kimlik Numarasına ait başka bir kullanıcı bulunmakta.';
+      } else if (error.toString().contains('ID_NOT_FOUND')) {
+        errorMessage = 'Bu TC Kimlik Numarasına ait kullanıcı bulunamadı.';
+      } else if (error.toString().contains('INVALID_PASSWORD')) {
+        errorMessage = 'Geçersiz şifre.';
+      }
+      _showErrorDialog(errorMessage);
+    } catch (error) {
+      const errorMessage =
+          'Sizi yetkilendiremedik. Lütfen daha sonra tekrar deneyin.';
+      _showErrorDialog(errorMessage);
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.Login) {
+      setState(() {
+        _authMode = AuthMode.Signup;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.Login;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 8.0,
+      child: Container(
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'TC Kimlik No'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value.isEmpty || value.length != 11) {
+                      return 'Hatalı TC Kimlik No!';
+                    }
+                  },
+                  onSaved: (value) {
+                    _authData['nationalIdentityNo'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Şifre'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return 'Şifre çok kısa!';
+                    }
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value;
+                  },
+                ),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    enabled: _authMode == AuthMode.Signup,
+                    decoration: InputDecoration(labelText: 'Şifreyi Onayla'),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return 'Şifreler eşleşmiyor!';
+                            }
+                          }
+                        : null,
+                  ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  ElevatedButton(
+                    child: Text(
+                        _authMode == AuthMode.Login ? 'GİRİŞ YAP' : 'KAYDOL'),
+                    onPressed: _submit,
+                    style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 30.0, vertical: 8.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        textStyle: TextStyle(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .button
+                                .color)),
+                  ),
+                TextButton(
+                  child: Text(
+                      'ŞİMDİ ${_authMode == AuthMode.Login ? 'KAYDOL' : 'GİRİŞ YAP'}'),
+                  onPressed: _switchAuthMode,
+                  style: TextButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                      textStyle:
+                          TextStyle(color: Theme.of(context).primaryColor),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
