7e6d6525f775f80550d7018644f31eff359932b0
ifiratli <ifiratli@gmail.com>
2021-04-15 00:01:52 +0300

    Left menu is updated.

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,19 +1,22 @@
 import 'package:aid_assist/providers/auth.dart';
+import 'package:aid_assist/screens/call_emergency_numbers_screen.dart';
+import 'package:aid_assist/screens/daily_corona_statistics_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainDrawer extends StatelessWidget {
   Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
       leading: Icon(
         icon,
-        size: 26,
+        size: 20,
       ),
       title: Text(
         title,
         style: TextStyle(
           fontFamily: 'RobotoCondensed',
-          fontSize: 24,
+          fontSize: 18,
           fontWeight: FontWeight.bold,
         ),
       ),
@@ -21,13 +24,27 @@ class MainDrawer extends StatelessWidget {
     );
   }
 
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
+        child: SingleChildScrollView(
       child: Column(
         children: <Widget>[
           Container(
-            height: 120,
+            height: 80,
             width: double.infinity,
             padding: EdgeInsets.all(20),
             alignment: Alignment.centerLeft,
@@ -41,9 +58,40 @@ class MainDrawer extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 20,
+            height: 1,
           ),
-          buildListTile('Ana Ekran', Icons.home, () {
+          buildListTile('Anasayfa', Icons.home, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Afet öncesi bilgilendirme', Icons.star, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Afet sonrası bilgilendirme', Icons.star, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Alo yardım', Icons.star, () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CallEmergencyNumbersScreen(),
+              ),
+            );
+          }),
+          buildListTile('Sağlık bakanlığı', Icons.star, () {
+            _launchInBrowser('https://www.saglik.gov.tr/');
+          }),
+          buildListTile('Korona önlem', Icons.star, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Güncel istatistik', Icons.star, () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DailyCoronaStatisticsScreen(),
+              ),
+            );
+          }),
+          buildListTile('Hakkımızda ', Icons.star, () {
             Navigator.of(context).pushReplacementNamed('/');
           }),
           Divider(),
@@ -58,6 +106,6 @@ class MainDrawer extends StatelessWidget {
           ),
         ],
       ),
-    );
+    ));
   }
 }
