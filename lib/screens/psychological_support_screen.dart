209dfaee12c007a9894d1a32a5042592771f3124
Aybuke KBezirci <aybuke.kbezirci@gmail.com>
2021-04-17 22:01:23 +0300

    Psychological support screen are updated.

--- lib/screens/psychological_support_screen.dart
+++ lib/screens/psychological_support_screen.dart
@@ -1,10 +1,50 @@
+import 'package:aid_assist/providers/auth.dart';
+import 'package:aid_assist/ui/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PsychologicalSupportScreen extends StatelessWidget {
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Psikolojik Destek Hattı'),
-    );
+    const number = "123";
+    return SingleChildScrollView(
+        child: Container(
+      child: Column(children: <Widget>[
+        newPsychologicalSupportCard(
+            "PSİKOLOJİK DESTEK ÜNİTESİ", Colors.white, () => {}),
+        newPsychologicalSupportCard(
+            "Afet süresince ortaya çıkabilecek psikolojik tepkiler nelerdir?",
+            Colors.black,
+            () => _launchInBrowser(
+                'http://' + Auth.hostAddress + "/static/afet_surecince.pdf")),
+        newPsychologicalSupportCard(
+            "Afet kaynaklı yaşadığınız psikolojik problemler için neler yapabilirsiniz?",
+            Colors.black,
+            () => _launchInBrowser('http://' +
+                Auth.hostAddress +
+                "/static/neler_yapilabilir.pdf")),
+        newPsychologicalSupportCard(
+            "Aile İl Sağlık Müdürlüğü Sağlıklı Hayat Merkezi Hızlı Erişim",
+            Colors.black,
+            () => launch("tel:03704333126")),
+        newPsychologicalSupportCard(
+            "Aile Çalışma ve Sosyal Hizmetler İl Müdürlüğü",
+            Colors.black,
+            () => launch("tel:03704122902")),
+      ]),
+    ));
   }
 }
--- lib/ui/widgets.dart
+++ lib/ui/widgets.dart
@@ -2,6 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'constants.dart';
 
+Card newPsychologicalSupportCard(String title, Color color, Function function) {
+  return Card(
+      color: Colors.green.withOpacity(0.8),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: InkWell(
+          onTap: () {
+            // Function is executed on tap.
+            function();
+          },
+          child: Center(
+              child: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, color: color),
+          )),
+        ),
+      ));
+}
+
 Card newCoronaStatisticsCard(String title, var number) {
   return Card(
     child: Column(
@@ -39,49 +58,49 @@ Card newCoronaStatisticsCard(String title, var number) {
 }
 
 Card newEmergencyCallCard(String title, var number) {
-   return Card(
-     child: Column(
-       mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ColoredBox(
-                    color: Colors.red,
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 20,
-                      child: Center(
-                          child: Text(
-                        title,
-                        style:
-                            TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-                      )),
-                    )),
-                ColoredBox(
-                  color: Color.fromRGBO(111, 111, 111, 0.80),
-                  child: SizedBox(
-                      width: double.infinity,
-                      height: 55,
-                      child: Center(
-                          child: new GestureDetector(
-                              onTap: () => launch("tel://" + number),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.call,
-                                    color: Colors.red,
-                                    size: 35,
-                                  ),
-                                  Text(
-                                    number,
-                                    style: TextStyle(
-                                        fontSize: 35,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  )
-                                ],
-                              )))),
-                ),
-              ],
-            ),
-          );
-        }
+  return Card(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ColoredBox(
+            color: Colors.red,
+            child: SizedBox(
+              width: double.infinity,
+              height: 20,
+              child: Center(
+                  child: Text(
+                title,
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              )),
+            )),
+        ColoredBox(
+          color: Color.fromRGBO(111, 111, 111, 0.80),
+          child: SizedBox(
+              width: double.infinity,
+              height: 55,
+              child: Center(
+                  child: new GestureDetector(
+                      onTap: () => launch("tel://" + number),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.call,
+                            color: Colors.red,
+                            size: 35,
+                          ),
+                          Text(
+                            number,
+                            style: TextStyle(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )
+                        ],
+                      )))),
+        ),
+      ],
+    ),
+  );
+}
